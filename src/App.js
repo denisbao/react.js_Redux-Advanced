5f1e22bf48de522bbbed21ac5b0e60b403960269
denisbao <denis.bao@cowmed.com.br>
2022-07-20 07:56:01 -0300

    259 - Bug correction and organizing App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 import Notification from './components/UI/Notification'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { uiActions } from './store/ui-slice'
 
 function App() {
@@ -12,30 +12,11 @@ function App() {
   const cart = useSelector(state => state.cart)
   const notification = useSelector(state => state.ui.notification)
 
-  // let isInitial = true
+  const isInitial = useRef(true)
 
   useEffect(() => {
-    async function sendCartData() {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending',
-          message: 'Sending cart data...',
-        })
-      )
-
-      const response = await fetch(
-        'https://redux-advanced-93b6c-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      )
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.')
-      }
-
+    //------------------------------------- Notification types
+    function notificateSuccess() {
       dispatch(
         uiActions.showNotification({
           status: 'success',
@@ -44,13 +25,7 @@ function App() {
         })
       )
     }
-
-    // if (isInitial) {
-    //   isInitial = false
-    //   return
-    // }
-
-    sendCartData().catch(error => {
+    function notificateError() {
       dispatch(
         uiActions.showNotification({
           status: 'error',
@@ -58,10 +33,45 @@ function App() {
           message: 'Sending cart data failed!',
         })
       )
+    }
+    function notiicatePending() {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending',
+          message: 'Sending cart data...',
+        })
+      )
+    }
+    //------------------------------------- Notification types
+
+    async function sendCartData() {
+      notiicatePending()
+      const response = await fetch(
+        'https://redux-advanced-93b6c-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      )
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.')
+      }
+      notificateSuccess()
+    }
+
+    // If is the firs time that the App is runing, we don't send the cart to Firebase.
+    if (isInitial.current) {
+      isInitial.current = false
+      return
+    }
+
+    // Send cart data to Firebase
+    sendCartData().catch(error => {
+      notificateError()
     })
   }, [cart, dispatch])
 
-  console.log(notification)
   return (
     <>
       {notification && (
